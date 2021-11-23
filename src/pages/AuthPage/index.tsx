ed07f8a556c4c04ed5169db321908b9d1ae0e122
Jerry Hogan <jerry@enyata.com>
2021-11-23 21:15:53 +0100

    Integrated github login

--- src/pages/AuthPage/index.tsx
+++ src/pages/AuthPage/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import { AuthCard, AuthWrapper } from './auth.style';
 import logo from '../../assets/icons/logo.svg';
+import LoginGithub from 'react-login-github';
 
 const AuthPage = () => {
+
+    const [clientId,] = React.useState('4f262cc9e20d3043da02');
+
+    const authorize = (data: object) => {
+        fetch('https://9uj0ihoex6.execute-api.eu-west-1.amazonaws.com/dev/auth',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(data => data.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    };
+
 
     return (
         <AuthWrapper>
@@ -11,7 +28,11 @@ const AuthPage = () => {
                     <img src={logo} alt='digilogo'/>
                     <span>Diga Search</span>
                 </p>
-                <button className="auth_btn">Login with Github</button>
+                <LoginGithub className="auth_btn" clientId={clientId}
+                    onSuccess={(data: object) => authorize(data)}
+                    onFailure={() => console.log('fail')}>
+                    Login with Github
+                </LoginGithub>
             </AuthCard>
         </AuthWrapper>
     )
